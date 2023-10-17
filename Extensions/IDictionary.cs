3b51179c725b82dbbb270817d7e005a80bfc800c
James Lawrence <jlaw90@hotmail.com>
2023-10-17 15:58:59 +0100

    Minor refactorings

--- Extensions/IDictionary.cs
+++ Extensions/IDictionary.cs
@@ -6,7 +6,6 @@ internal static class DictionaryFallbackExtension
 {
     public static TV GetValue<TK, TV>(this IDictionary<TK, TV> dict, TK key, TV defaultValue = default(TV))
     {
-        TV value;
-        return dict.TryGetValue(key, out value) ? value : defaultValue;
+        return dict.TryGetValue(key, out var value) ? value : defaultValue;
     }
 }
--- IO/BinaryReader.cs
+++ IO/BinaryReader.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace XboxLib.IO;
 
